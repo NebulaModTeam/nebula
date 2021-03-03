d8fb62a4aa36c8197b24e6b052f44545a5b66978
flapperkewiet <r0737258@student.thomasmore.be>
2021-03-03 10:03:11 +0100

    changed tabs to spaces

--- NebulaClient/GameLogic/PlayerManager.cs
+++ NebulaClient/GameLogic/PlayerManager.cs
@@ -5,80 +5,80 @@ using NebulaModel.DataStructures;
 
 namespace NebulaClient.GameLogic
 {
-	public class PlayerManager
-	{
-		Dictionary<ushort, Player> remotePlayers;
-		Dictionary<ushort, RemotePlayerModel> remotePlayerModels;
+    public class PlayerManager
+    {
+        Dictionary<ushort, Player> remotePlayers;
+        Dictionary<ushort, RemotePlayerModel> remotePlayerModels;
 
-		public Player LocalPlayer { get; protected set; }
-		public readonly LocalPlayerModel LocalPlayerModel = new LocalPlayerModel();
+        public Player LocalPlayer { get; protected set; }
+        public readonly LocalPlayerModel LocalPlayerModel = new LocalPlayerModel();
 
-		public PlayerManager()
-		{
-			remotePlayers = new Dictionary<ushort, Player>();
-			remotePlayerModels = new Dictionary<ushort, RemotePlayerModel>();
-		}
+        public PlayerManager()
+        {
+            remotePlayers = new Dictionary<ushort, Player>();
+            remotePlayerModels = new Dictionary<ushort, RemotePlayerModel>();
+        }
 
-		public void SetLocalPlayer(ushort localPlayerId)
-		{
-			LocalPlayer = new Player(localPlayerId);
+        public void SetLocalPlayer(ushort localPlayerId)
+        {
+            LocalPlayer = new Player(localPlayerId);
 
-			// For now give the player a random color
-			LocalPlayer.UpdateColor(new Float4(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1.0f));
-		}
+            // For now give the player a random color
+            LocalPlayer.UpdateColor(new Float4(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1.0f));
+        }
 
-		public void AddRemotePlayer(ushort playerId)
-		{
-			if (remotePlayers.ContainsKey(playerId))
-			{
-				Log.Error($"RemotePlayerManager :: Already contains the playerId {playerId}");
-				return;
-			}
+        public void AddRemotePlayer(ushort playerId)
+        {
+            if (remotePlayers.ContainsKey(playerId))
+            {
+                Log.Error($"RemotePlayerManager :: Already contains the playerId {playerId}");
+                return;
+            }
 
-			Player info = new Player(playerId);
-			remotePlayers.Add(playerId, info);
+            Player info = new Player(playerId);
+            remotePlayers.Add(playerId, info);
 
-			RemotePlayerModel model = new RemotePlayerModel(playerId);
-			remotePlayerModels.Add(playerId, model);
-		}
+            RemotePlayerModel model = new RemotePlayerModel(playerId);
+            remotePlayerModels.Add(playerId, model);
+        }
 
-		public void RemovePlayer(ushort playerId)
-		{
-			remotePlayers.Remove(playerId);
+        public void RemovePlayer(ushort playerId)
+        {
+            remotePlayers.Remove(playerId);
 
-			if (remotePlayerModels.ContainsKey(playerId))
-			{
-				remotePlayerModels[playerId].Destroy();
-				remotePlayerModels.Remove(playerId);
-			}
-		}
+            if (remotePlayerModels.ContainsKey(playerId))
+            {
+                remotePlayerModels[playerId].Destroy();
+                remotePlayerModels.Remove(playerId);
+            }
+        }
 
-		public void RemoveAll()
-		{
-			foreach (var playerModel in remotePlayerModels.Values)
-			{
-				playerModel.Destroy();
-			}
-			remotePlayerModels.Clear();
-			remotePlayers.Clear();
-		}
+        public void RemoveAll()
+        {
+            foreach (var playerModel in remotePlayerModels.Values)
+            {
+                playerModel.Destroy();
+            }
+            remotePlayerModels.Clear();
+            remotePlayers.Clear();
+        }
 
-		public Player GetPlayerById(ushort playerId)
-		{
-			if (remotePlayers.ContainsKey(playerId))
-			{
-				return remotePlayers[playerId];
-			}
-			return null;
-		}
+        public Player GetPlayerById(ushort playerId)
+        {
+            if (remotePlayers.ContainsKey(playerId))
+            {
+                return remotePlayers[playerId];
+            }
+            return null;
+        }
 
-		public RemotePlayerModel GetPlayerModelById(ushort playerId)
-		{
-			if (remotePlayerModels.ContainsKey(playerId))
-			{
-				return remotePlayerModels[playerId];
-			}
-			return null;
-		}
-	}
+        public RemotePlayerModel GetPlayerModelById(ushort playerId)
+        {
+            if (remotePlayerModels.ContainsKey(playerId))
+            {
+                return remotePlayerModels[playerId];
+            }
+            return null;
+        }
+    }
 }
--- NebulaModel/Packets/Players/PlayerColorChanged.cs
+++ NebulaModel/Packets/Players/PlayerColorChanged.cs
@@ -2,16 +2,16 @@
 
 namespace NebulaModel.Packets.Players
 {
-	public class PlayerColorChanged
-	{
-		public ushort PlayerId { get; set; }
-		public Float4 Color { get; set; }
+    public class PlayerColorChanged
+    {
+        public ushort PlayerId { get; set; }
+        public Float4 Color { get; set; }
 
-		public PlayerColorChanged() { }
-		public PlayerColorChanged(ushort playerID, Float4 color) 
-		{
-			this.PlayerId = playerID;
-			this.Color = color;
-		}
-	}
+        public PlayerColorChanged() { }
+        public PlayerColorChanged(ushort playerID, Float4 color)
+        {
+            this.PlayerId = playerID;
+            this.Color = color;
+        }
+    }
 }
