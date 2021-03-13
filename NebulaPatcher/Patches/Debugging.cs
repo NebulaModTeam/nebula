31814cd90e07aa0f086e5fc6888125a7b2a61b44
Hubert Gendron <hubert.gendron689@gmail.com>
2021-03-13 18:31:57 -0500

    Add some debug patch to be able to fly from a fresh save

---
+++ NebulaPatcher/Patches/Debugging.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace NebulaPatcher.Patches
+{
+    [HarmonyPatch(typeof(GameHistoryData), "EnqueueTech")]
+    class patch
+    {
+        public static void Postfix(GameHistoryData __instance, int techId)
+        {
+            __instance.UnlockTech(techId);
+            GameMain.mainPlayer.mecha.corePowerGen = 10000000;
+        }
+    }
+
+    [HarmonyPatch(typeof(Mecha), "UseWarper")]
+    class patch2
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
