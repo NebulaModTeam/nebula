12f62766e0545d7f6f9028281592694e5f57b75e
Baldie-dev <lukas.michalec11@gmail.com>
2021-03-23 07:43:33 +0100

    Fix for the landing pod

---
+++ NebulaPatcher/Patches/Dynamic/GuideMissionStandardMode_Patch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using NebulaWorld;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(GuideMissionStandardMode), "Skip")]
+    class GuideMissionStandardMode_Patch
+    {
+        public static bool Prefix()
+        {
+            //This prevents spawning landing capsule and preparing spawn area for the clients in multiplayer.
+            return !SimulatedWorld.Initialized || LocalPlayer.IsMasterClient;
+        }
+    }
+}
