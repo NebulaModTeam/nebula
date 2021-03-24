98a6dfab954266c339bf812095cf45a517081fed
Baldie-dev <lukas.michalec11@gmail.com>
2021-03-24 19:29:57 +0100

    Added comments and Log statements

--- NebulaClient/PacketProcessors/Universe/DysonSphereDataProcessor.cs
+++ NebulaClient/PacketProcessors/Universe/DysonSphereDataProcessor.cs
@@ -5,6 +5,7 @@ using NebulaModel.Packets.Processors;
 using System.IO;
 using LZ4;
 using System.IO.Compression;
+using NebulaModel.Logger;
 
 namespace NebulaClient.PacketProcessors.Universe
 {
@@ -13,15 +14,21 @@ namespace NebulaClient.PacketProcessors.Universe
     {
         public void ProcessPacket(DysonSphereData packet, NebulaConnection conn)
         {
+            //Failsafe, if client does not have instantiated sphere for the star, it will create dummy one that will be replaced during import
             if (GameMain.data.dysonSpheres[packet.StarIndex] == null)
             {
                 GameMain.data.dysonSpheres[packet.StarIndex] = new DysonSphere();
                 GameMain.data.statistics.production.Init(GameMain.data);
-                GameMain.data.statistics.production.factoryStatPool[0] = new FactoryProductionStat();
-                GameMain.data.statistics.production.factoryStatPool[0].Init();
+                //Another failsafe, DysonSphere import requires initialized factory statistics
+                if (GameMain.data.statistics.production.factoryStatPool[0] == null)
+                {
+                    GameMain.data.statistics.production.factoryStatPool[0] = new FactoryProductionStat();
+                    GameMain.data.statistics.production.factoryStatPool[0].Init();
+                }
                 GameMain.data.dysonSpheres[packet.StarIndex].Init(GameMain.data, GameMain.data.galaxy.stars[packet.StarIndex]);
             }
 
+            Log.Info($"Parsing {packet.BinaryData.Length} bytes of data for DysonSphere in system {GameMain.data.galaxy.stars[packet.StarIndex].name} (Index: {GameMain.data.galaxy.stars[packet.StarIndex].index})");
             using (MemoryStream ms = new MemoryStream(packet.BinaryData))
             using (LZ4Stream ls = new LZ4Stream(ms, CompressionMode.Decompress))
             using (BufferedStream bs = new BufferedStream(ls, 8192))
