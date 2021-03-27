51ea7e573cf78f41566e63f2a45dab291b447fbb
Baldie-dev <lukas.michalec11@gmail.com>
2021-03-27 14:24:33 +0100

    Removing print statements

--- NebulaClient/PacketProcessors/Universe/DysonSphereAddNodeProcessor.cs
+++ NebulaClient/PacketProcessors/Universe/DysonSphereAddNodeProcessor.cs
@@ -16,17 +16,14 @@ namespace NebulaClient.PacketProcessors.Universe
             Log.Info($"Processing DysonSphere Add Node notification for system {GameMain.data.galaxy.stars[packet.StarIndex].name} (Index: {GameMain.data.galaxy.stars[packet.StarIndex].index})");
             DysonSphere_Manager.IncomingDysonSpherePacket = true;
             int? addedID = GameMain.data.dysonSpheres[packet.StarIndex]?.GetLayer(packet.LayerId)?.NewDysonNode(packet.NodeProtoId, DataStructureExtensions.ToUnity(packet.Position));
-            Log.Info($"Added node: {addedID}");
             //Try to add frames that failed due to the missing nodes
             DysonSphereLayer dsl = GameMain.data.dysonSpheres[packet.StarIndex]?.GetLayer(packet.LayerId);
             DysonSphereAddFramePacket queuedPacked;
             for (int i = DysonSphere_Manager.QueuedAddFramePackets.Count - 1; i >= 0; i--)
             {
                 queuedPacked = DysonSphere_Manager.QueuedAddFramePackets[i];
-                Log.Info($"Procesing queue, try to add frame {queuedPacked.NodeAId } - {queuedPacked.NodeBId} ");
                 if (dsl?.nodePool[queuedPacked.NodeAId]?.id != 0 && dsl?.nodePool[queuedPacked.NodeBId]?.id != 0)
                 {
-                    Log.Info($"find! added!");
                     dsl.NewDysonFrame(queuedPacked.ProtoId, queuedPacked.NodeAId, queuedPacked.NodeBId, queuedPacked.Euler);
                     DysonSphere_Manager.QueuedAddFramePackets.RemoveAt(i);
                 }
