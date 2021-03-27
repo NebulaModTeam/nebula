17799f6b4c1d7a6aae6a56ad794c458a476fb5f3
Hubert Gendron <hubert.gendron689@gmail.com>
2021-03-26 23:56:08 -0400

    Quick patch for the last PR that was merge. I will need to look at fixing it.

--- NebulaPatcher/Patches/Dynamic/GameMain_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/GameMain_Patch.cs
@@ -7,9 +7,14 @@ namespace NebulaPatcher.Patches.Dynamic
     {
         public static bool Prefix()
         {
+            // TODO: We cannot really do that since it will prevent the Esc Menu from opening and the
+            // players won't be able to exit the game or change their game settings. We should instead
+            // Wopen the menu, but unpause the game under the hood.
+            return true;
+
             //Do not pause game in the multiplayer
             //Pausing game has to be done via: GameMain.instance._paused = true;
-            return !SimulatedWorld.Initialized;
+            //return !SimulatedWorld.Initialized;
         }
     }
 }
