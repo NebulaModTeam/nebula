6221bc5ff018a2a5fe21a2041f997190d9084e00
Baldie-dev <lukas.michalec11@gmail.com>
2021-03-29 21:45:52 +0200

    GameHistoryData sync + Realtime research sync

---
+++ NebulaHost/PacketProcessors/GameHistory/GameHistoryDataRequestProcessor.cs
@@ -0,0 +1,29 @@
+﻿using LZ4;
+using NebulaModel.Attributes;
+using NebulaModel.Networking;
+using NebulaModel.Packets.GameHistory;
+using NebulaModel.Packets.Processors;
+using System.IO;
+using System.IO.Compression;
+
+namespace NebulaHost.PacketProcessors.GameHistory
+{
+    [RegisterPacketProcessor]
+    public class GameHistoryDataRequestProcessor : IPacketProcessor<GameHistoryDataRequest>
+    {
+        public void ProcessPacket(GameHistoryDataRequest packet, NebulaConnection conn)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            using (MemoryStream ms2 = new MemoryStream())
+            {
+                using (LZ4Stream ls = new LZ4Stream(ms, CompressionMode.Compress))
+                using (BufferedStream bs = new BufferedStream(ls, 8192))
+                using (BinaryWriter bw = new BinaryWriter(bs))
+                {
+                    GameMain.history.Export(bw);
+                }
+                conn.SendPacket(new GameHistoryDataResponse(ms.ToArray()));
+            }
+        }
+    }
+}
---
+++ NebulaModel/Packets/GameHistory/GameHistoryResearchUpdatePacket.cs
@@ -0,0 +1,16 @@
+﻿namespace NebulaModel.Packets.GameHistory
+{
+    public class GameHistoryResearchUpdatePacket
+    {
+        public int TechId { get; set; }
+        public long HashUploaded { get; set; }
+
+        public GameHistoryResearchUpdatePacket() { }
+
+        public GameHistoryResearchUpdatePacket(int techId, long hashUploaded) 
+        {
+            this.TechId = techId;
+            this.HashUploaded = hashUploaded;
+        }
+    }
+}
