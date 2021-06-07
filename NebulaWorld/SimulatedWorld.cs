df0da7b0f10cac57ffaddb123cc21e25ab180b0c
sp00ktober <ezra.knobloch@protonmail.com>
2021-06-07 21:06:31 +0200

    fix for #313 (missing null check)

--- NebulaWorld/SimulatedWorld.cs
+++ NebulaWorld/SimulatedWorld.cs
@@ -284,13 +284,17 @@ namespace NebulaWorld
                 }
                 else
                 {
+                    Debug.Log("removing " + packet.VegeId);
                     VegeData vegeData = factory.GetVegeData(packet.VegeId);
                     VegeProto vegeProto = LDB.veges.Select((int)vegeData.protoId);
 
                     factory.RemoveVegeWithComponents(packet.VegeId);
 
-                    VFEffectEmitter.Emit(vegeProto.MiningEffect, vegeData.pos, Maths.SphericalRotation(vegeData.pos, 0f));
-                    VFAudio.Create(vegeProto.MiningAudio, null, vegeData.pos, true);
+                    if(vegeProto != null)
+                    {
+                        VFEffectEmitter.Emit(vegeProto.MiningEffect, vegeData.pos, Maths.SphericalRotation(vegeData.pos, 0f));
+                        VFAudio.Create(vegeProto.MiningAudio, null, vegeData.pos, true);
+                    }
                 }
             }
             else if(factory != null)
