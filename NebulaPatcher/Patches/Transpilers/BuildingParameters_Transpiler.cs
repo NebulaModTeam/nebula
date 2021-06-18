3121b66b6e4d12e32f8141c477c9cc813527c50a
Christopher Yeninas <phantom@sundergaming.com>
2021-06-18 07:28:45 -0400

    Create BuildingParameters_Transpiler.cs
    Patch parts that would add items to inventory to only occur for the player that did the action

---
+++ NebulaPatcher/Patches/Transpilers/BuildingParameters_Transpiler.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using NebulaWorld;
+using NebulaWorld.Factory;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NebulaPatcher.Patches.Transpilers
+{
+    [HarmonyPatch(typeof(BuildingParameters))]
+    public class BuildingParameters_Transpiler
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(BuildingParameters.PasteToFactoryObject))]
+        public static IEnumerable<CodeInstruction> PasteToFactoryObject_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator iL)
+        {
+            /*
+             * Replaces
+             *      factory.TakeBackItemsInEntity(mainPlayer, objectId);
+             * With
+             *      if(!SimulatedWorld.Initialized || (!FactoryManager.EventFromServer && !FactoryManager.EventFromClient))
+             *      {
+             *         factory.TakeBackItemsInEntity(mainPlayer, objectId);
+             *      }
+            */
+            var codeMatcher = new CodeMatcher(instructions, iL)
+                .MatchForward(false,
+                    new CodeMatch(i => i.IsLdarg()),
+                    new CodeMatch(i => i.IsLdloc()),
+                    new CodeMatch(i => i.IsLdloc()),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "TakeBackItemsInEntity"));
+
+            if (codeMatcher.IsInvalid)
+            {
+                NebulaModel.Logger.Log.Error("BuildingParameters.PasteToFactoryObject TakeBackItemsInEntity transpiler failed");
+                return instructions;
+            }
+
+            instructions = codeMatcher
+            .Repeat(matcher =>
+            {
+                matcher
+                    .CreateLabelAt(matcher.Pos + 4, out Label label)
+                    .InsertAndAdvance(HarmonyLib.Transpilers.EmitDelegate<Func<bool>>(() =>
+                    {
+                        return !SimulatedWorld.Initialized || (!FactoryManager.EventFromServer && !FactoryManager.EventFromClient);
+                    }))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Brfalse, label))
+                    .Advance(5);
+            })
+            .InstructionEnumeration();
+
+            /*
+             * Replaces
+             *      factorySystem.TakeBackItems_[Assembler||Lab](mainPlayer, [assemblerId||labId]);
+             * With
+             *      if(!SimulatedWorld.Initialized || (!FactoryManager.EventFromServer && !FactoryManager.EventFromClient))
+             *      {
+             *         factorySystem.TakeBackItems_[Assembler||Lab](mainPlayer, [assemblerId||labId]);
+             *      }
+            */
+            codeMatcher = new CodeMatcher(instructions, iL)
+                .MatchForward(false,
+                    new CodeMatch(i => i.IsLdloc()),
+                    new CodeMatch(i => i.IsLdloc()),
+                    new CodeMatch(i => i.IsLdloc()),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && (((MethodInfo)i.operand).Name == "TakeBackItems_Assembler" || ((MethodInfo)i.operand).Name == "TakeBackItems_Lab")));
+
+            if (codeMatcher.IsInvalid)
+            {
+                NebulaModel.Logger.Log.Error("BuildingParameters.PasteToFactoryObject TakeBackItems_[Assembler||Lab] transpiler failed");
+                return instructions;
+            }
+
+            return codeMatcher
+            .Repeat(matcher =>
+            {
+                matcher
+                    .CreateLabelAt(matcher.Pos + 4, out Label label)
+                    .InsertAndAdvance(HarmonyLib.Transpilers.EmitDelegate<Func<bool>>(() =>
+                    {
+                        return !SimulatedWorld.Initialized || (!FactoryManager.EventFromServer && !FactoryManager.EventFromClient);
+                    }))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Brfalse, label))
+                    .Advance(5);
+            })
+            .InstructionEnumeration();
+        }
+    }
+}
