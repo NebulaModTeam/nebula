2da47131791b1693a4a530fa9e205eb6388cb7fc
Christopher Yeninas <phantom@sundergaming.com>
2021-06-18 02:43:20 -0400

    patch Mecha AddConsumptionStat and AddProductionStat
    client can't reliably get nearestPlanet, so skip this for now to avoid nre

--- NebulaPatcher/Patches/Dynamic/Mecha_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/Mecha_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using NebulaWorld;
+using NebulaWorld.Factory;
 
 namespace NebulaPatcher.Patches.Dynamic
 {
@@ -14,6 +15,22 @@ namespace NebulaPatcher.Patches.Dynamic
             {
                 SimulatedWorld.OnDronesGameTick(time, dt);
             }
+        }
+
+        // We can't do this as client as we won't be able to get_nearestPlanet() since we do not currently have all of the factory info
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Mecha), nameof(Mecha.AddConsumptionStat))]
+        [HarmonyPatch(typeof(Mecha), nameof(Mecha.AddProductionStat))]
+        public static bool AddStat_Common_Prefix(ref PlanetFactory factory)
+        {
+            if(!SimulatedWorld.Initialized || LocalPlayer.IsMasterClient)
+                return true;
+
+            // TODO: Send packet to host to add stat?
+            // Easy option: just have host add stat to their closest planet, though is this better than not syncing it at all?
+            // Hard option: find a way to reliably get nearest planet from client with missing data
+
+            return false;
         }
     }
 }
