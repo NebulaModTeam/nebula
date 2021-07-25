8877b1ed522c3febe712883f3a4500a53a6bdf22
Chris Yeninas <phantom@sundergaming.com>
2021-07-25 18:44:38 -0400

    Add changelog to thunderstore release readme.md

--- .github/scripts/thunderstore_bundle.js
+++ .github/scripts/thunderstore_bundle.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
+import { existsSync, mkdirSync, appendFileSync, readFileSync, writeFileSync, copyFileSync, readdirSync, statSync } from 'fs';
+import { join } from 'path';
 
 const DIST_FOLDER = 'dist\\thunderstore\\nebula';
 const PLUGIN_INFO = 'NebulaPatcher\\PluginInfo.cs'
 const MOD_ICON = 'thunderstore_icon.png';
 const README = 'README.md';
+const CHANGELOG = 'CHANGELOG.md';
 const NEBULA_BINARIES = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Dyson Sphere Program\\BepInEx\\plugins\\Nebula';
 
 function main() {
-    if (!fs.existsSync(DIST_FOLDER)) {
-        fs.mkdirSync(DIST_FOLDER, {recursive: true})
+    if (!existsSync(DIST_FOLDER)) {
+        mkdirSync(DIST_FOLDER, {recursive: true})
     }
 
     generateManifest()
     copyIcon()
     copyReadme()
+    appendChangelog()
     copyFolderContent(NEBULA_BINARIES, DIST_FOLDER, ['.pdb'])
     copyLicenses()
 }
 
 function getPluginInfo() {
-    const pluginInfoRaw = fs.readFileSync(PLUGIN_INFO).toString("utf-8")
+    const pluginInfoRaw = readFileSync(PLUGIN_INFO).toString("utf-8")
     return {
         name: pluginInfoRaw.match(/PLUGIN_NAME = "(.*)";/)[1],
         id: pluginInfoRaw.match(/PLUGIN_ID = "(.*)";/)[1],
@@ -39,34 +41,38 @@ function generateManifest() {
         dependencies: ["xiaoye97-BepInEx-5.4.11"],
         website_url: "https://github.com/hubastard/nebula"
     }
-    fs.writeFileSync(path.join(DIST_FOLDER, 'manifest.json'), JSON.stringify(manifest, null, 2))
+    writeFileSync(join(DIST_FOLDER, 'manifest.json'), JSON.stringify(manifest, null, 2))
 }
 
 function copyIcon() {
-    fs.copyFileSync(MOD_ICON, path.join(DIST_FOLDER, 'icon.png'))
+    copyFileSync(MOD_ICON, join(DIST_FOLDER, 'icon.png'))
 }
 
 function copyReadme() {
-    fs.copyFileSync(README, path.join(DIST_FOLDER, 'README.md'))
+    copyFileSync(README, join(DIST_FOLDER, README))
+}
+
+function appendChangelog() {
+    appendFileSync(join(DIST_FOLDER, README), "\n" + readFileSync(CHANGELOG))
 }
 
 function copyLicenses() {
-    fs.copyFileSync('LICENSE', path.join(DIST_FOLDER, 'nebula.LICENSE'))
+    copyFileSync('LICENSE', join(DIST_FOLDER, 'nebula.LICENSE'))
     copyFolderContent('Licenses', DIST_FOLDER)
 }
 
 function copyFolderContent(src, dst, excludedExts) {
-    fs.readdirSync(src).forEach(file => {
-        const srcPath = path.join(src, file)
-        const dstPath = path.join(dst, file)
-        if (fs.statSync(srcPath).isDirectory() ) {
-            if (!fs.existsSync(dstPath)) {
-                fs.mkdirSync(dstPath)
+    readdirSync(src).forEach(file => {
+        const srcPath = join(src, file)
+        const dstPath = join(dst, file)
+        if (statSync(srcPath).isDirectory() ) {
+            if (!existsSync(dstPath)) {
+                mkdirSync(dstPath)
                 copyFolderContent(srcPath, dstPath, excludedExts)
             }
         } else {
             if (!excludedExts || !excludedExts.includes(file.substr(file.lastIndexOf('.')))) {
-                fs.copyFileSync(srcPath, dstPath)
+                copyFileSync(srcPath, dstPath)
             }
         }
     })
---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+0.3.1:
+* Fixed issue where if client didn't have enough items to upgrade, the buildings would still be upgraded for the host.
+* Clients will now retain their detail display settings between sessions (e.g. power grid visibility) (thanks to @Needix)
+* Fixed issue where players would be able to construct buildings made with blueprints even if they did not have the required items.
+* Fixed miscellaneous issues related to ILS ship movement
+
+0.3.0:
+* Added support for blueprint update (0.8.x)
+* Improved player name tag rendering
+* Fixed newly introduced multithread issues
+
+0.2.0:
+* initial release on thunderstore
