eeb04e29606e20a4552bbe47b524ca55696050db
sp00ktober <ezra.knobloch@protonmail.com>
2021-10-04 10:26:50 +0200

    smoother planet loading for clients

---
+++ NebulaPatcher/Patches/Dynamic/RaycastLogic_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using NebulaWorld;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(RaycastLogic))]
+    internal class RaycastLogic_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(RaycastLogic.GameTick))]
+        public static bool GameTick_Prefix(RaycastLogic __instance)
+        {
+            if (Multiplayer.IsActive && Multiplayer.Session.LocalPlayer.IsClient)
+            {
+                if(__instance.factory == null)
+                {
+                    // while we wait for factory data from server this is still null, prevent running into NRE
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
