db2e36a95aa1d918552a7c6259ab9b5df4fbb42d
sp00ktober <ezra.knobloch@protonmail.com>
2021-10-30 23:58:48 +0200

    dont rely on GPUInstancingManager.activeFactory (or planet) as we change that due to issue #462. Instead only rely on the real local planet.

--- NebulaPatcher/Patches/Dynamic/GPUInstancingManager_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/GPUInstancingManager_Patch.cs
@@ -41,7 +41,7 @@ namespace NebulaPatcher.Patches.Dynamic
         public static bool AddModel_Prefix(GPUInstancingManager __instance, ref int __result)
         {
             //Do not add model to the GPU queue if player is not on the same planet as building that was build
-            if (Multiplayer.IsActive && Multiplayer.Session.Factories.EventFactory != null && Multiplayer.Session.Factories.EventFactory.planet != __instance.activePlanet)
+            if (Multiplayer.IsActive && Multiplayer.Session.Factories.EventFactory != null && Multiplayer.Session.Factories.EventFactory.planet != GameMain.localPlanet)
             {
                 __result = 0;
                 return false;
@@ -54,7 +54,7 @@ namespace NebulaPatcher.Patches.Dynamic
         public static bool AddPrebuildModel_Prefix(GPUInstancingManager __instance, ref int __result)
         {
             //Do not add model to the GPU queue if player is not on the same planet as building that was build
-            if (Multiplayer.IsActive && Multiplayer.Session.Factories.EventFactory != null && Multiplayer.Session.Factories.EventFactory.planet != __instance.activePlanet)
+            if (Multiplayer.IsActive && Multiplayer.Session.Factories.EventFactory != null && Multiplayer.Session.Factories.EventFactory.planet != GameMain.localPlanet)
             {
                 __result = 0;
                 return false;
