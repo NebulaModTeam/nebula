f2bd8f1525d299abba12c266197add15bbdefc41
sp00ktober <ezra.knobloch@protonmail.com>
2021-11-24 21:18:36 +0100

    Avoid index out of bounds by skipping a method. This is a quickfix but needs to be replaced by a proper syncing of the warning system introduced in the latest game patches.

---
+++ NebulaPatcher/Patches/Dynamic/WarningSystem_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(WarningSystem))]
+    class WarningSystem_Patch
+    {
+        /*
+         * Until we have a proper syncing of this system we avoid index out of bounds by skipping the method if needed.
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(WarningSystem), nameof(WarningSystem.RemoveWarningData))]
+        public static bool RemoveWarningData_Prefix(WarningSystem __instance, int id)
+        {
+            if(id > __instance.warningCursor)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
