6b4cd5bb84610be902cb8f3bb1ed50f8361423de
kremnev8 <kremnev8super@yandex.ru>
2022-01-15 22:38:26 +0300

    Fix VFInput in unity editor

---
+++ NebulaUnity/Assets/Scripts/InitDSPCode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitDSPCode : MonoBehaviour
+{
+    private void Awake()
+    {
+        VFInput.Init();
+    }
+    
+    private void Update()
+    {
+        VFInput.OnUpdate();
+    }
+
+}
