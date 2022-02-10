5bf7af64522b94e04bf1a88e248d87850ec1e74a
Matt Semar <mattsemar@gmail.com>
2022-02-10 12:13:45 -0600

    Add support for command aliases. Clear chatBox after sending command

--- NebulaWorld/Chat/Commands/ChatCommandRegistry.cs
+++ NebulaWorld/Chat/Commands/ChatCommandRegistry.cs
@@ -7,43 +7,73 @@ namespace NebulaWorld.Chat.Commands
     public static class ChatCommandRegistry
     {
         public const string CommandPrefix = "/";
-        private static Dictionary<string, IChatCommandHandler> commands = new Dictionary<string, IChatCommandHandler>();
+        private static readonly Dictionary<ChatCommandKey, IChatCommandHandler> commands = new Dictionary<ChatCommandKey, IChatCommandHandler>();
 
-        public static void RegisterCommand(string commandName, IChatCommandHandler commandHandlerHandler)
+        public static void RegisterCommand(string commandName, IChatCommandHandler commandHandlerHandler, params string[] aliases)
         {
             if (commandHandlerHandler == null) return;
-            if (commands.ContainsKey(commandName))
+            if (NameOrAliasRegistered(commandName, aliases))
             {
                 Log.Debug($"Can't register command, because command for {commandName} was already registered!");
                 return;
             }
             
             Log.Debug($"Registering command handler for {commandName}");
-            commands.Add(commandName, commandHandlerHandler);
+            commands.Add(new ChatCommandKey(commandName, aliases), commandHandlerHandler);
         }
         
-        public static IChatCommandHandler GetCommandHandler(string commandName)
+        public static IChatCommandHandler GetCommandHandler(string commandOrAlias)
         {
-            if (commands.ContainsKey(commandName))
-            {
-                return commands[commandName];
-            }
-
-            return null;
+            ChatCommandKey chatCommandKey = commands.Keys
+                .FirstOrDefault((command) => command.RespondsTo(commandOrAlias.ToLowerInvariant()));
+            return chatCommandKey != null ? commands[chatCommandKey] : null;
         }
 
         public static IChatCommandHandler[] GetCommands()
         {
             return commands.Values.ToArray();
         }
+        private static bool NameOrAliasRegistered(string commandName, string[] aliases)
+        {
+            if (commands.Keys.Any(command => command.RespondsTo(commandName)))
+            {
+                return true;
+            }
+
+            HashSet<string> aliasesSet = new HashSet<string>(aliases);
+            foreach (ChatCommandKey command in commands.Keys)
+            {
+                if (aliasesSet.Overlaps(command.Aliases))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         static ChatCommandRegistry()
         {
             RegisterCommand("ping", new PingCommandHandler());
-            RegisterCommand("help", new HelpCommandHandler());
-            RegisterCommand("who", new WhoCommandHandler());
+            RegisterCommand("help", new HelpCommandHandler(), "h", "?");
+            RegisterCommand("who", new WhoCommandHandler(), "players", "list");
         }
-        
-        
+    }
+
+    internal class ChatCommandKey
+    {
+        public readonly string Name;
+        public readonly HashSet<string> Aliases;
+
+        public ChatCommandKey(string commandName, string[] aliases)
+        {
+            Name = commandName;
+            Aliases = new HashSet<string>(aliases);
+        }
+
+        public bool RespondsTo(string nameOrAlias)
+        {
+            return Name == nameOrAlias || Aliases.Contains(nameOrAlias);
+        }
     }
 }
