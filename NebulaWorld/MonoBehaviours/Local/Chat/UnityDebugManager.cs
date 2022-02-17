455e13f35897648284f09e69cc3c3d87ecf38132
kremnev8 <kremnev8super@yandex.ru>
2022-02-17 07:04:03 +0300

    Fix cyclic unity build error

---
+++ NebulaWorld/MonoBehaviours/Local/Chat/UnityDebugManager.cs
@@ -0,0 +1,44 @@
+﻿using NebulaModel.Logger;
+using UnityEngine;
+using ILogger = NebulaModel.Logger.ILogger;
+
+namespace NebulaWorld.MonoBehaviours.Local
+{
+    public class UnityDebugManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            VFInput.Init();
+            Log.Init(new EditorLogger());
+        }
+
+        private void Update()
+        {
+            VFInput.OnUpdate();
+        }
+    }
+
+
+    public class EditorLogger : ILogger
+    {
+        public void LogDebug(object data)
+        {
+            Debug.Log(data);
+        }
+
+        public void LogInfo(object data)
+        {
+            Debug.Log(data);
+        }
+
+        public void LogWarning(object data)
+        {
+            Debug.LogWarning(data);
+        }
+
+        public void LogError(object data)
+        {
+            Debug.LogError(data);
+        }
+    }
+}
