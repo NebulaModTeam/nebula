0b037d56bfbbf46e88c76890cf6643ebe49d7653
starfish <ac355deny@gmail.com>
2022-04-23 02:48:08 +0800

    Add fractionator, powerGenerator product update packet and inserter buffer item update packet
    - Fix an issue where inserter's filter doesn't set correctly

---
+++ NebulaNetwork/PacketProcessors/Factory/Inserter/InserterItemUpdatePacket.cs
@@ -0,0 +1,23 @@
+﻿using NebulaAPI;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Factory.Inserter;
+
+namespace NebulaNetwork.PacketProcessors.Factory.Inserter
+{
+    [RegisterPacketProcessor]
+    internal class InserterItemUpdateProcessor : PacketProcessor<InserterItemUpdatePacket>
+    {
+        public override void ProcessPacket(InserterItemUpdatePacket packet, NebulaConnection conn)
+        {
+            InserterComponent[] pool = GameMain.galaxy.PlanetById(packet.PlanetId).factory?.factorySystem.inserterPool;
+            if (pool != null && packet.InserterIndex != -1 && packet.InserterIndex < pool.Length && pool[packet.InserterIndex].id != -1)
+            {
+                pool[packet.InserterIndex].itemId = packet.ItemId;
+                pool[packet.InserterIndex].itemCount = packet.ItemCount;
+                pool[packet.InserterIndex].itemInc = packet.ItemInc;
+                pool[packet.InserterIndex].stackCount = packet.StackCount;
+            }
+        }
+    }
+}
