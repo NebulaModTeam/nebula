1d6fc854ccf27e2a6671490814c1b11d3e3fbf88
sp00ktober <ezra.knobloch@protonmail.com>
2022-04-25 00:43:44 +0200

    add graceful error messages for corner cases. wip chat reconnect command.

---
+++ NebulaPatcher/Patches/Dynamic/VFPreload_Patch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using NebulaModel;
+using NebulaWorld.GameStates;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(VFPreload))]
+    internal class VFPreload_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VFPreload.InvokeOnLoadWorkEnded))]
+        public static void InvokeOnLoadWorkEnded_Postfix()
+        {
+            if (GameStatesManager.DuringReconnect)
+            {
+                string ip = "127.0.0.1:8469";
+                if (Config.Options.RememberLastIP && !string.IsNullOrWhiteSpace(Config.Options.LastIP))
+                {
+                    ip = Config.Options.LastIP;
+                }
+
+                UIMainMenu_Patch.JoinGame(ip);
+                GameStatesManager.DuringReconnect = false;
+            }
+        }
+    }
+}
---
+++ NebulaWorld/Chat/Commands/ReconnectCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using NebulaModel.DataStructures;
+using NebulaWorld.GameStates;
+using NebulaWorld.MonoBehaviours.Local;
+
+namespace NebulaWorld.Chat.Commands
+{
+    public class ReconnectCommandHandler : IChatCommandHandler
+    {
+        public void Execute(ChatWindow window, string[] parameters)
+        {
+            if (!Multiplayer.IsActive || Multiplayer.Session.LocalPlayer.IsHost)
+            {
+                window.SendLocalChatMessage("This command can only be used in multiplayer and as client!", ChatMessageType.CommandErrorMessage);
+                return;
+            }
+            GameStatesManager.DoFastReconnect();
+        }
+
+        public string[] GetUsage()
+        {
+            return new string[] { $"" };
+        }
+
+        public string GetDescription()
+        {
+            return "performs a reconnect.";
+        }
+    }
+}
