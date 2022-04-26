47fe4462ea54a77f4bba4de066aeca3f0004e116
starfish <ac355deny@gmail.com>
2022-04-27 05:52:49 +0800

    Add milestone syncing
    sync when client join and when a milestone is unlocked

---
+++ NebulaNetwork/PacketProcessors/Statistics/MilestoneUnlockProcessor.cs
@@ -0,0 +1,44 @@
+﻿using NebulaAPI;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Statistics;
+using NebulaWorld;
+
+namespace NebulaNetwork.PacketProcessors.Statistics
+{
+    [RegisterPacketProcessor]
+    internal class MilestoneUnlockProcessor : PacketProcessor<MilestoneUnlockPacket>
+    {
+        public override void ProcessPacket(MilestoneUnlockPacket packet, NebulaConnection conn)
+        {
+            IPlayerManager playerManager = Multiplayer.Session.Network.PlayerManager;
+            bool valid = true;
+
+            if (IsHost)
+            {
+                INebulaPlayer player = playerManager.GetPlayer(conn);
+                if (player != null)
+                {
+                    playerManager.SendPacketToOtherPlayers(packet, player);
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+
+            if (valid)
+            {
+                using (Multiplayer.Session.Statistics.IsIncomingRequest.On())
+                {
+                    if (GameMain.data.milestoneSystem.milestoneDatas.TryGetValue(packet.Id, out MilestoneData milestoneData))
+                    {
+                        milestoneData.journalData.patternId = packet.PatternId;
+                        milestoneData.journalData.parameters = packet.Parameters;
+                        GameMain.data.milestoneSystem.UnlockMilestone(packet.Id, packet.UnlockTick);
+                    }
+                }
+            }
+        }
+    }
+}
