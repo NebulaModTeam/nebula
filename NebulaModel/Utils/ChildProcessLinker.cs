c612e8ca535a94d4d03908de1aa7f586bb3f85f8
MMjr <2429824+mmjr-x@users.noreply.github.com>
2022-05-08 17:26:55 +0200

    Moved the ChildProcessLinker to NebulaModel/Utils and did some cleanup

--- NebulaModel/Utils/ChildProcessLinker.cs
+++ NebulaModel/Utils/ChildProcessLinker.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace NebulaNetwork.Ngrok
+namespace NebulaModel.Utils
 {
 
     // Adapted from https://stackoverflow.com/a/24012744/13620003
