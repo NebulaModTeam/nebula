14cc26473923a238b9b71cce2a222ebc06b5118d
starfish <ac355deny@gmail.com>
2022-05-14 05:27:52 +0800

    Suppress abnormality reports on client

---
+++ NebulaPatcher/Patches/Dynamic/UIAbnormalityTip.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using NebulaWorld;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(UIAbnormalityTip))]
+    internal class UIAbnormalityTip_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(UIAbnormalityTip._OnOpen))]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Original Function Name")]
+        public static void _OnOpen_Prefix(UIAbnormalityTip __instance)
+        {
+            // Suppress runtime errror reports on client because client can't unlock achievements anyway in host's save
+            if (Multiplayer.IsActive && Multiplayer.Session.LocalPlayer.IsClient)
+            {
+                __instance._Close();
+            }
+        }
+    }
+}
