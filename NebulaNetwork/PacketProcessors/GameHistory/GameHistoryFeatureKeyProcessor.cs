ade467903ab73fe69a53ab1333ee5ac7e3466e0d
starfish <ac355deny@gmail.com>
2022-06-11 23:44:59 +0800

    Add dyson sphere quick build syncing

---
+++ NebulaNetwork/PacketProcessors/GameHistory/GameHistoryFeatureKeyProcessor.cs
@@ -0,0 +1,38 @@
+﻿using NebulaAPI;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.GameHistory;
+using NebulaWorld;
+
+namespace NebulaNetwork.PacketProcessors.GameHistory
+{
+    [RegisterPacketProcessor]
+    internal class GameHistoryFeatureKeyProcessor : PacketProcessor<GameHistoryFeatureKeyPacket>
+    {
+        public override void ProcessPacket(GameHistoryFeatureKeyPacket packet, NebulaConnection conn)
+        {
+            if (IsHost)
+            {
+                Multiplayer.Session.Network.SendPacketExclude(packet, conn);
+            }
+
+            using (Multiplayer.Session.History.IsIncomingRequest.On())
+            {
+                if (packet.Add)
+                {
+                    GameMain.data.history.RegFeatureKey(packet.FeatureId);
+                }
+                else
+                {
+                    GameMain.data.history.UnregFeatureKey(packet.FeatureId);
+                }
+
+                if (packet.FeatureId == 1100002)
+                {
+                    // Update Quick Build button in dyson editor
+                    UIRoot.instance.uiGame.dysonEditor.controlPanel.inspector.overview.autoConstructSwitch.SetImmediately(packet.Add);
+                }
+            }
+        }
+    }
+}
