a6152bf7ec452bfbba2e95ec68ac6439802be2b7
starfish <ac355deny@gmail.com>
2022-06-13 06:01:17 +0800

    Temporary fix for fast traveling
    Temporary stop client from teleporting to gas giant to prevent error.
    There must be something different from other teleport mods that cause official teleport to error.

---
+++ NebulaPatcher/Patches/Transpilers/PostEffectController_Transpiler.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace NebulaPatcher.Patches.Transpiler
+{
+    [HarmonyPatch(typeof(PostEffectController))]
+    internal class PostEffectController_Transpiler
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(PostEffectController.Update))]
+        private static IEnumerable<CodeInstruction> Update_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            // TODO:   Find out the root cause of fast travel error
+            // Change: if (localPlanet.type != EPlanetType.Gas)
+            // To:     if (localPlanet.type != EPlanetType.Gas && localPlanet.factoryloaded)
+
+            CodeMatcher matcher = new CodeMatcher(instructions, il)
+                .MatchForward(true,
+                    new CodeMatch(i => i.IsLdloc()),
+                    new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(PlanetData), nameof(PlanetData.type))),
+                    new CodeMatch(OpCodes.Ldc_I4_5),
+                    new CodeMatch(OpCodes.Beq)
+                );
+
+            if (matcher.IsInvalid)
+            {
+                NebulaModel.Logger.Log.Error("PostEffectController.Update_Transpiler failed. Mod version not compatible with game version.");
+                return instructions;
+            }
+
+            object jumpOperand = matcher.Instruction.operand;
+
+            return matcher
+                    .Advance(1)
+                    .InsertAndAdvance(HarmonyLib.Transpilers.EmitDelegate<Func<bool>>(() =>
+                    {
+                        return GameMain.localPlanet.factoryLoaded;
+                    }))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Brfalse, jumpOperand))
+                    .InstructionEnumeration();
+        }
+    }
+}
