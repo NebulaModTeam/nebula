b5145b2372a3d1183a433017f42ef9575f7b32a5
starfish <ac355deny@gmail.com>
2022-06-13 01:31:58 +0800

    Add ThrowSolarSailLogic_Sandbox syncing

--- NebulaPatcher/Patches/Transpilers/PlayerMove_Sail_Transpiler.cs
+++ NebulaPatcher/Patches/Transpilers/PlayerMove_Sail_Transpiler.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using NebulaModel.Packets.Players;
+using NebulaModel.Packets.Universe;
 using NebulaWorld;
 using System;
 using System.Collections.Generic;
@@ -56,5 +57,33 @@ namespace NebulaPatcher.Patches.Transpilers
                 .InstructionEnumeration();
         }
 
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(PlayerMove_Sail.ThrowSolarSailLogic_Sandbox))]
+        public static IEnumerable<CodeInstruction> ThrowSolarSailLogic_Sandbox_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            // Send DysonSailDataPacket whenever calling dysonSphere.swarm.AddSolarSail()
+
+            CodeMatcher codeMatcher = new CodeMatcher(instructions)
+                .MatchForward(true,
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "AddSolarSail")
+                );
+
+            if (codeMatcher.IsInvalid)
+            {
+                NebulaModel.Logger.Log.Error("PlayerMove_Sail_Transpiler.ThrowSolarSailLogic_Sandbox failed. Mod version not compatible with game version.");
+                return instructions;
+            }
+
+            return codeMatcher
+                .SetInstruction (HarmonyLib.Transpilers.EmitDelegate<Func<DysonSwarm, DysonSail, int, long, int>>((swarm, sail, orbitId, expiryTime) =>
+                {
+                    if (Multiplayer.IsActive)
+                    {
+                        Multiplayer.Session.Network.SendPacket(new DysonSailDataPacket(swarm.dysonSphere.starData.index, ref sail, orbitId, expiryTime));
+                    }
+                    return swarm.AddSolarSail(sail, orbitId, expiryTime);
+                }))
+                .InstructionEnumeration();
+        }
     }
 }
