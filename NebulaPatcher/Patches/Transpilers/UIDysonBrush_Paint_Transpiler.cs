a0f232b1ddd548061d8565809446dcc7b62db9fb
starfish <50672801+starfi5h@users.noreply.github.com>
2022-12-18 09:26:31 +0800

    Fix dyson color brush error in single player mode

--- NebulaPatcher/Patches/Transpilers/UIDysonBrush_Paint_Transpiler.cs
+++ NebulaPatcher/Patches/Transpilers/UIDysonBrush_Paint_Transpiler.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using NebulaModel.Packets.Universe;
 using NebulaWorld;
+using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
@@ -14,10 +15,11 @@ namespace NebulaPatcher.Patches.Transpilers
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Original Function Name")]
         public static IEnumerable<CodeInstruction> _OnUpdate_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            try {
+            try
+            {
                 int pos1, pos2;
                 CodeMatcher matcher = new CodeMatcher(instructions);
-                
+
                 matcher.MatchForward(false, new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(DysonNode), "color")));
                 pos1 = matcher.Pos;
                 matcher.MatchBack(false, new CodeMatch(OpCodes.Ldloc_S));
@@ -25,7 +27,14 @@ namespace NebulaPatcher.Patches.Transpilers
                 matcher.Advance(pos1 - pos2 + 1)
                     .Insert(
                         new CodeInstruction(matcher.InstructionAt(pos2 - pos1 - 1)),
-                        new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UIDysonBrush_Paint_Transpiler), "SendNodePacket"))
+                        HarmonyLib.Transpilers.EmitDelegate<Action<DysonNode>>((node) =>
+                        {
+                            if (Multiplayer.IsActive)
+                            {
+                                int starIndex = UIRoot.instance.uiGame.dysonEditor.selection.viewStar.index;
+                                Multiplayer.Session.Network.SendPacket(new DysonSphereColorChangePacket(starIndex, node.layerId, node.color, DysonSphereColorChangePacket.ComponentType.Node, node.id));
+                            }
+                        })
                     );
 
                 matcher.MatchForward(false, new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(DysonFrame), "color")));
@@ -35,7 +44,14 @@ namespace NebulaPatcher.Patches.Transpilers
                 matcher.Advance(pos1 - pos2 + 1)
                     .Insert(
                         new CodeInstruction(matcher.InstructionAt(pos2 - pos1 - 1)),
-                        new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UIDysonBrush_Paint_Transpiler), "SendFramePacket"))
+                        HarmonyLib.Transpilers.EmitDelegate<Action<DysonFrame>>((frame) =>
+                        {
+                            if (Multiplayer.IsActive)
+                            {
+                                int starIndex = UIRoot.instance.uiGame.dysonEditor.selection.viewStar.index;
+                                Multiplayer.Session.Network.SendPacket(new DysonSphereColorChangePacket(starIndex, frame.layerId, frame.color, DysonSphereColorChangePacket.ComponentType.Frame, frame.id));
+                            }
+                        })
                     );
 
                 matcher.MatchForward(false, new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(DysonShell), "color")));
@@ -45,7 +61,14 @@ namespace NebulaPatcher.Patches.Transpilers
                 matcher.Advance(pos1 - pos2 + 1)
                     .Insert(
                         new CodeInstruction(matcher.InstructionAt(pos2 - pos1 - 1)),
-                        new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UIDysonBrush_Paint_Transpiler), "SendShellPacket"))
+                        HarmonyLib.Transpilers.EmitDelegate<Action<DysonShell>>((shell) =>
+                        {
+                            if (Multiplayer.IsActive)
+                            {
+                                int starIndex = UIRoot.instance.uiGame.dysonEditor.selection.viewStar.index;
+                                Multiplayer.Session.Network.SendPacket(new DysonSphereColorChangePacket(starIndex, shell.layerId, shell.color, DysonSphereColorChangePacket.ComponentType.Shell, shell.id));
+                            }
+                        })
                     );
 
                 return matcher.InstructionEnumeration();
@@ -55,24 +78,6 @@ namespace NebulaPatcher.Patches.Transpilers
                 NebulaModel.Logger.Log.Error("UIDysonBrush_Paint._OnUpdate_Transpiler failed. Mod version not compatible with game version.");
                 return instructions;
             }
-        }
-
-        public static void SendNodePacket(DysonNode node)
-        {
-            int starIndex = UIRoot.instance.uiGame.dysonEditor.selection.viewStar.index;
-            Multiplayer.Session.Network.SendPacket(new DysonSphereColorChangePacket(starIndex, node.layerId, node.color, DysonSphereColorChangePacket.ComponentType.Node, node.id));
-        }
-
-        public static void SendFramePacket(DysonFrame frame)
-        {
-            int starIndex = UIRoot.instance.uiGame.dysonEditor.selection.viewStar.index;
-            Multiplayer.Session.Network.SendPacket(new DysonSphereColorChangePacket(starIndex, frame.layerId, frame.color, DysonSphereColorChangePacket.ComponentType.Frame, frame.id));
-        }
-
-        public static void SendShellPacket(DysonShell shell)
-        {
-            int starIndex = UIRoot.instance.uiGame.dysonEditor.selection.viewStar.index;
-            Multiplayer.Session.Network.SendPacket(new DysonSphereColorChangePacket(starIndex, shell.layerId, shell.color, DysonSphereColorChangePacket.ComponentType.Shell, shell.id));
         }
     }
 }
