439d302f37b680400cc5bb132238120cd6022087
starfish <50672801+starfi5h@users.noreply.github.com>
2023-02-02 01:34:23 +0800

    Sync UIBeltWindow.OnReverseButtonClick

---
+++ NebulaModel/Packets/Factory/Belt/BeltReversePacket.cs
@@ -0,0 +1,15 @@
+﻿namespace NebulaModel.Packets.Factory.Belt
+{
+    public class BeltReversePacket
+    {
+        public int BeltId { get; set; }
+        public int PlanetId { get; set; }
+
+        public BeltReversePacket() { }
+        public BeltReversePacket(int beltId, int planetId)
+        {
+            BeltId = beltId;
+            PlanetId = planetId;
+        }
+    }
+}
---
+++ NebulaPatcher/Patches/Dynamic/UIBeltWindow_Patch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using NebulaModel.Packets.Factory.Belt;
+using NebulaWorld;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(UIBeltWindow))]
+    internal class UIBeltWindow_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIBeltWindow), nameof(UIBeltWindow.OnReverseButtonClick))]
+        public static void OnReverseButtonClick_Postfix(UIBeltWindow __instance)
+        {
+            // Notify others about belt direction reverse
+            if (Multiplayer.IsActive && !Multiplayer.Session.Factories.IsIncomingRequest.Value)
+            {
+                Multiplayer.Session.Network.SendPacketToLocalStar(new BeltReversePacket(__instance.beltId, __instance.factory.planetId));
+            }
+        }
+    }
+}
