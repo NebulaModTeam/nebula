5a091ac233662aa7f7db72db4e39a4e67db562af
starfish <50672801+starfi5h@users.noreply.github.com>
2023-07-09 13:10:14 +0800

    Disable OnEntityBuild & OnEntityDismantle audio when entity is too far from player

---
+++ NebulaPatcher/Patches/Dynamic/FactoryAudio_Patch.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using NebulaWorld;
+using UnityEngine;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch(typeof(FactoryAudio))]
+    class FactoryAudio_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(FactoryAudio.OnEntityBuild))]
+        public static bool OnEntityBuild_Prefix(FactoryAudio __instance, int objId)
+        {
+            if (Multiplayer.IsActive && Multiplayer.Session.Factories.IsIncomingRequest.Value && objId > 0)
+            {
+                Vector3 pos = __instance.planet.factory.entityPool[objId].pos;
+                NebulaModel.Logger.Log.Debug((pos - GameMain.mainPlayer.position).sqrMagnitude + " " + (__instance.planet.radius * __instance.planet.radius / 4));
+                if ((pos - GameMain.mainPlayer.position).sqrMagnitude > (__instance.planet.radius * __instance.planet.radius / 4))
+                {
+                    // Don't make sounds if distance is over half of the planet radius
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(FactoryAudio.OnEntityDismantle))]
+        public static bool OnEntityDismantle_Prefix(FactoryAudio __instance, int objId)
+        {
+            if (Multiplayer.IsActive && Multiplayer.Session.Factories.IsIncomingRequest.Value)
+            {
+                Vector3 pos;
+                if (objId > 0)
+                {
+                    pos = __instance.planet.factory.entityPool[objId].pos;
+                }
+                else
+                {
+                    pos = __instance.planet.factory.prebuildPool[-objId].pos;
+                }
+                if ((pos - GameMain.mainPlayer.position).sqrMagnitude > (__instance.planet.radius * __instance.planet.radius / 4))
+                {
+                    // Don't make sounds if distance is over half of the planet radius
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
