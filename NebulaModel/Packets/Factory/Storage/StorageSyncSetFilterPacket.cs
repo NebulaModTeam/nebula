fc4434ae3a155b173012e078982e1d78c817be10
Karim Rizk <karim@rizk.dev>
2023-12-21 08:26:57 +0200

    Update comment

--- NebulaModel/Packets/Factory/Storage/StorageSyncSetFilterPacket.cs
+++ NebulaModel/Packets/Factory/Storage/StorageSyncSetFilterPacket.cs
@@ -1,11 +1,9 @@
-﻿namespace NebulaModel.Packets.Factory.Storage;
+namespace NebulaModel.Packets.Factory.Storage;
 
 public class StorageSyncSetFilterPacket
 {
     public StorageSyncSetFilterPacket() { }
 
-    // @TODO: This can potentially be moved up the call stack - in UI input - for storage boxes-
-    // to avoid sending redundant data like `StorageIndex` & `StorageType` per every grid slot.
     public StorageSyncSetFilterPacket(int storageIndex, int planetId, int gridIndex, int filterId, EStorageType storageType)
     {
         StorageIndex = storageIndex;
