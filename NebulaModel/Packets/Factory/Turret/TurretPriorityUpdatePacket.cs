23c63f88201da7f461147a99f88b720894728a8c
Zzarek <31546035+Zzarek@users.noreply.github.com>
2023-12-27 11:58:06 -0600

    Debatably working priority update

---
+++ NebulaModel/Packets/Factory/Turret/TurretPriorityUpdatePacket.cs
@@ -0,0 +1,17 @@
+﻿namespace NebulaModel.Packets.Factory.Turret;
+
+public class TurretPriorityUpdatePacket
+{
+    public TurretPriorityUpdatePacket() { }
+
+    public TurretPriorityUpdatePacket(int turretIndex, VSLayerMask vsSettings, int planetId)
+    {
+        TurretIndex = turretIndex;
+        VSSettings = vsSettings;
+        PlanetId = planetId;
+    }
+
+    public int TurretIndex { get; set; }
+    public VSLayerMask VSSettings { get; set; }
+    public int PlanetId { get; set; }
+}
