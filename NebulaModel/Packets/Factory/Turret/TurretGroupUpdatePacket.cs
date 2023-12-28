1d3dd13cbfee262a5fc885ffea43dcd22aaddc4f
Zzarek <31546035+Zzarek@users.noreply.github.com>
2023-12-28 11:11:22 -0600

    Initial Packets

---
+++ NebulaModel/Packets/Factory/Turret/TurretGroupUpdatePacket.cs
@@ -0,0 +1,17 @@
+﻿namespace NebulaModel.Packets.Factory.Turret;
+
+public class TurretGroupUpdatePacket
+{
+    public TurretGroupUpdatePacket() { }
+
+    public TurretGroupUpdatePacket(int turretIndex, byte group, int planetId)
+    {
+        TurretIndex = turretIndex;
+        Group = group;
+        PlanetId = planetId;
+    }
+
+    public int TurretIndex { get; set; }
+    public byte Group { get; set; }
+    public int PlanetId { get; set; }
+}
---
+++ NebulaModel/Packets/Factory/Turret/TurretStorageUpdatePacket.cs
@@ -0,0 +1,21 @@
+﻿namespace NebulaModel.Packets.Factory.Turret;
+
+public class TurretStorageUpdatePacket
+{
+    public TurretStorageUpdatePacket() { }
+
+    public TurretStorageUpdatePacket(int turretIndex, int itemId, short itemCount, short itemInc, int planetId)
+    {
+        TurretIndex = turretIndex;
+        ItemId = itemId;
+        ItemCount = itemCount;
+        ItemInc = itemInc;
+        PlanetId = planetId;
+    }
+
+    public int TurretIndex { get; set; }
+    public int ItemId { get; set; }
+    public short ItemCount { get; set; }
+    public short ItemInc { get; set; }
+    public int PlanetId { get; set; }
+}
