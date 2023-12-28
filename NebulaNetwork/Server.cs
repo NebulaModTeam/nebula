fe7f520358ecdf1780389ba967a919f7b6140395
Karim Rizk <karim@rizk.dev>
2023-12-28 11:38:35 +0200

    Small fix

--- NebulaNetwork/Server.cs
+++ NebulaNetwork/Server.cs
@@ -196,6 +196,7 @@ public class Server : NetworkProvider, IServer
     public override void Stop()
     {
         CancellationTokenSource.Cancel();
+        runningTasks.Clear();
 
         socket?.Stop();
 
