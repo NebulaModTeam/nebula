b0985afebb43551c6beb3addc2080448d8248a79
starfish <50672801+starfi5h@users.noreply.github.com>
2024-01-03 15:27:27 +0800

    Fix packet logging message

--- NebulaModel/Networking/Serialization/NebulaNetPacketProcessor.cs
+++ NebulaModel/Networking/Serialization/NebulaNetPacketProcessor.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using NebulaAPI.Interfaces;
@@ -44,7 +44,7 @@ public class NebulaNetPacketProcessor : NetPacketProcessor
 #if DEBUG
         if (!typeof(T).IsDefined(typeof(HidePacketInDebugLogsAttribute), false))
         {
-            Log.Debug($"Packet Sent: {packet.GetType().Name}, Size: {writer.Data.Length}");
+            Log.Debug($"Packet Sent << {packet.GetType().Name}, Size: {writer.Length}");
         }
 #endif
 
@@ -106,7 +106,6 @@ public class NebulaNetPacketProcessor : NetPacketProcessor
                 var packet = new PendingPacket(rawData, userData);
                 var dueTime = DateTime.UtcNow.AddMilliseconds(simulationRandom.Next(SimulatedMinLatency, SimulatedMaxLatency));
                 delayedPackets.Add(new DelayedPacket(packet, dueTime));
-                Log.Debug($"Received packet of size: {rawData.Length}");
                 return;
             }
         }
