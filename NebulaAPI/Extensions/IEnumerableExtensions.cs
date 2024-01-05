b120462f9a58a08cd88abd5d57f56cb6853b4e1b
Karim Rizk <karim@rizk.dev>
2024-01-05 11:55:09 +0200

    Add IEnumerable ForEach as an extension

---
+++ NebulaAPI/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿// #pragma once
+// #ifndef IEnumerableExtensions.cs_H_
+// #define IEnumerableExtensions.cs_H_
+// 
+// #endif
+
+using System;
+using System.Collections.Generic;
+
+namespace NebulaAPI.Extensions;
+
+public static class IEnumerableExtensions
+{
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        if (source is null) throw new ArgumentNullException(nameof(source));
+        if (action is null) throw new ArgumentNullException(nameof(action));
+
+        foreach (var item in source)
+        {
+            action(item);
+        }
+
+        return source;
+    }
+}
