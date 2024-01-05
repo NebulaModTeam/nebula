333f2bcc41e532431039a5f356f6b8c52a4ab82b
Karim Rizk <karim@rizk.dev>
2024-01-05 14:04:30 +0200

    Remove `UpdateSandCount` from player manager
    Update patch accordingly

--- NebulaPatcher/Patches/Dynamic/Player_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/Player_Patch.cs
@@ -2,6 +2,7 @@
 
 using HarmonyLib;
 using NebulaAPI;
+using NebulaAPI.Extensions;
 using NebulaModel;
 using NebulaModel.Packets.Players;
 using NebulaWorld;
@@ -24,6 +25,7 @@ internal class Player_Patch
                    Multiplayer.Session.Factories.PacketAuthor == NebulaModAPI.AUTHOR_NONE ||
                    !Multiplayer.Session.Factories.IsIncomingRequest.Value;
         }
+
         switch (Multiplayer.IsActive)
         {
             //Soil should be given in singleplayer or to the host who then syncs it back to all players.
@@ -31,8 +33,8 @@ internal class Player_Patch
                 var deltaSandCount = (int)(newSandCount - GameMain.mainPlayer.sandCount);
                 if (deltaSandCount != 0)
                 {
-                    Multiplayer.Session.Network.PlayerManager.UpdateSyncedSandCount(deltaSandCount);
-                    Multiplayer.Session.Network.SendPacket(new PlayerSandCount(newSandCount));
+                    UpdateSyncedSandCount(newSandCount - GameMain.mainPlayer.sandCount);
+                    Multiplayer.Session.Server.SendPacket(new PlayerSandCount(newSandCount));
                 }
                 break;
             //Or client that use reform tool
@@ -40,8 +42,25 @@ internal class Player_Patch
                 Multiplayer.Session.Network.SendPacket(new PlayerSandCount(newSandCount));
                 break;
         }
+
         return !Multiplayer.IsActive || Multiplayer.Session.LocalPlayer.IsHost;
         //Soil should be given in singleplayer or to the player who is author of the "Build" request, or to the host if there is no author.
+    }
+
+    private static void UpdateSyncedSandCount(long deltaSandCount)
+    {
+        var connectedPlayers = Multiplayer.Session.Server.Players.GetConnected();
+        foreach (var player in connectedPlayers)
+        {
+            player.Data.Mecha.SandCount += deltaSandCount / (connectedPlayers.Count + 1);
+            // dont be too picky here, a little bit more or less sand is ignorable i guess
+            if (player.Data.Mecha.SandCount < 0)
+            {
+                player.Data.Mecha.SandCount = 0;
+            }
+        }
+
+        Multiplayer.Session.LocalPlayer.Data.Mecha.SandCount += deltaSandCount / (connectedPlayers.Count + 1);
     }
 
     [HarmonyPrefix]
@@ -59,9 +78,9 @@ internal class Player_Patch
         {
             return true;
         }
+
         __result = 0;
         return false;
-
     }
 
     [HarmonyPrefix]
@@ -80,8 +99,8 @@ internal class Player_Patch
         {
             return true;
         }
+
         __result = 1;
         return false;
-
     }
 }
