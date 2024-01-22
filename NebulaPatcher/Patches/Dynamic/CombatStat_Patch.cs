4e6f530e5f11a940ae16c39db593bd372e24ee97
starfish <50672801+starfi5h@users.noreply.github.com>
2024-01-22 23:16:06 +0800

    Add CombatManger. Set LockBuildHp = true in MP

---
+++ NebulaPatcher/Patches/Dynamic/CombatStat_Patch.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using HarmonyLib;
+
+#endregion
+
+namespace NebulaPatcher.Patches.Dynamic;
+
+[HarmonyPatch(typeof(CombatStat))]
+internal class CombatStat_Patch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(CombatStat.TickSkillLogic))]
+    public static void TickSkillLogic_Prefix(ref CombatStat __instance)
+    {
+        if (NebulaWorld.Combat.CombatManager.LockBuildHp)
+        {
+            if (__instance.objectType == 0)
+            {
+                __instance.hp = __instance.hpMax;
+            }
+        }
+    }
+}
