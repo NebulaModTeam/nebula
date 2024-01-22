ca5faabe8608822c0b84dc5aa68b05120f7538a9
starfish <50672801+starfi5h@users.noreply.github.com>
2024-01-22 21:43:29 +0800

    Inital SpaceSector syncing

---
+++ NebulaNetwork/PacketProcessors/Combat/SpaceSectorResponseDataProcessor.cs
@@ -0,0 +1,29 @@
+﻿#region
+
+using NebulaAPI.Packets;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Combat;
+
+
+#endregion
+
+namespace NebulaNetwork.PacketProcessors.Combat;
+
+[RegisterPacketProcessor]
+internal class SpaceSectorResponseDataProcessor : PacketProcessor<SpaceSectorResponseDataPacket>
+{
+    protected override void ProcessPacket(SpaceSectorResponseDataPacket packet, NebulaConnection conn)
+    {
+        if (IsHost)
+        {
+            return;
+        }
+
+        using (var reader = new BinaryUtils.Reader(packet.SpaceSectorResponseData))
+        {
+            GameMain.data.spaceSector.Import(reader.BinaryReader);
+        }
+        GameMain.mainPlayer.mecha.CheckCombatModuleDataIsValidPatch();
+    }
+}
