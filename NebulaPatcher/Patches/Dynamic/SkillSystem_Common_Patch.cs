30c8b0813c0ed91ee97b0cf52c32d2be3053e512
starfish <50672801+starfi5h@users.noreply.github.com>
2024-02-09 01:40:39 +0800

    Switch skillSystem.playerSkillColliderL

--- NebulaPatcher/Patches/Dynamic/SkillSystem_Common_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/SkillSystem_Common_Patch.cs
@@ -49,14 +49,53 @@ internal class SkillSystem_Common_Patch
         skillSystem.playerSkillCastRightU = playerData.skillTargetU;
     }
 
+    static void SwtichTargetPlayerWithCollider(int playerId)
+    {
+        if (!Multiplayer.Session.Combat.IndexByPlayerId.TryGetValue(playerId, out var index)) return;
+
+        ref var playerData = ref Multiplayer.Session.Combat.Players[index];
+        var skillSystem = GameMain.data.spaceSector.skillSystem;
+        skillSystem.mecha = playerData.mecha;
+        skillSystem.playerSkillTargetL = playerData.skillTargetL;
+        skillSystem.playerSkillTargetULast = playerData.skillTargetULast;
+        skillSystem.playerSkillTargetU = playerData.skillTargetU;
+        skillSystem.playerSkillColliderL = playerData.mecha.skillColliderL;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(GeneralExpImpProjectile), nameof(GeneralExpImpProjectile.TickSkillLogic))]
+    public static void GeneralExpImpProjectile_Prefix(ref GeneralExpImpProjectile __instance)
+    {
+        if (!Multiplayer.IsActive) return;
+        if (__instance.caster.type == ETargetType.Player) SwtichPlayerState(__instance.caster.id);
+        if (__instance.target.type == ETargetType.Player) SwtichTargetPlayerWithCollider(__instance.target.id);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(GeneralMissile), nameof(GeneralMissile.TickSkillLogic))]
+    public static void GeneralProjectile_Prefix(ref GeneralMissile __instance)
+    {
+        if (!Multiplayer.IsActive) return;
+        if (__instance.caster.type == ETargetType.Player) SwtichPlayerState(__instance.caster.id);
+        if (__instance.target.type == ETargetType.Player) SwtichTargetPlayerWithCollider(__instance.target.id);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(GeneralProjectile), nameof(GeneralProjectile.TickSkillLogic))]
+    public static void GeneralProjectile_Prefix(ref GeneralProjectile __instance)
+    {
+        if (!Multiplayer.IsActive) return;
+        if (__instance.caster.type == ETargetType.Player) SwtichPlayerState(__instance.caster.id);
+        if (__instance.target.type == ETargetType.Player) SwtichTargetPlayerWithCollider(__instance.target.id);
+    }
+
     [HarmonyPrefix]
     [HarmonyPatch(typeof(LocalGeneralProjectile), nameof(LocalGeneralProjectile.TickSkillLogic))]
-
     public static void LocalGeneralProjectile_Prefix(ref LocalGeneralProjectile __instance)
     {
         if (!Multiplayer.IsActive) return;
         if (__instance.caster.type == ETargetType.Player) SwtichPlayerState(__instance.caster.id);
-        if (__instance.target.type == ETargetType.Player) SwtichPlayerState(__instance.target.id);
+        if (__instance.target.type == ETargetType.Player) SwtichTargetPlayerWithCollider(__instance.target.id);
     }
 
     [HarmonyPrefix]
@@ -86,15 +125,6 @@ internal class SkillSystem_Common_Patch
     }
 
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(GeneralProjectile), nameof(GeneralProjectile.TickSkillLogic))]
-    public static void GeneralProjectile_Prefix(ref GeneralProjectile __instance)
-    {
-        if (!Multiplayer.IsActive) return;
-        if (__instance.caster.type == ETargetType.Player) SwtichPlayerState(__instance.caster.id);
-        if (__instance.target.type == ETargetType.Player) SwtichPlayerState(__instance.target.id);
-    }
-
-    [HarmonyPrefix]
     [HarmonyPatch(typeof(SpaceLaserOneShot), nameof(SpaceLaserOneShot.TickSkillLogic))]
     public static void SpaceLaserOneShot_Prefix(ref SpaceLaserOneShot __instance)
     {
@@ -109,5 +139,6 @@ internal class SkillSystem_Common_Patch
     {
         if (!Multiplayer.IsActive) return;
         if (__instance.caster.type == ETargetType.Player) SwtichPlayerState(__instance.caster.id);
+        if ((__instance.mask & ETargetTypeMask.Player) != 0) SwtichTargetPlayerWithCollider(Multiplayer.Session.LocalPlayer.Id);
     }
 }
