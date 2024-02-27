df3964000e85dc58097942b2579329664ee38126
starfish <50672801+starfi5h@users.noreply.github.com>
2024-02-27 11:23:33 +0800

    Temporary disable UIDFCommunicatorWindow

--- NebulaWorld/Combat/EnemyManager.cs
+++ NebulaWorld/Combat/EnemyManager.cs
@@ -19,6 +19,8 @@ public class EnemyManager : IDisposable
 
     public readonly Dictionary<int, int[]> GroundTargets = new();
 
+    public const bool DISABLE_DFCommunicator = true;
+
     private readonly Dictionary<int, DFGUpdateBaseStatusPacket> basePackets = [];
     private readonly Dictionary<int, DFHiveUpdateStatusPacket> hivePackets = [];
 
