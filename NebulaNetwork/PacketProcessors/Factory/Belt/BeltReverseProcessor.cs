d42e3778a14dbeac35f4c73ec286d87ff1e56bc0
starfish <50672801+starfi5h@users.noreply.github.com>
2024-03-11 13:02:00 +0800

    Fix NRE when reversing belts with item on them

--- NebulaNetwork/PacketProcessors/Factory/Belt/BeltReverseProcessor.cs
+++ NebulaNetwork/PacketProcessors/Factory/Belt/BeltReverseProcessor.cs
@@ -1,18 +1,21 @@
 ﻿#region
 
+using System;
 using NebulaAPI;
 using NebulaAPI.Networking;
 using NebulaAPI.Packets;
+using NebulaModel.Logger;
 using NebulaModel.Packets.Factory.Belt;
+using NebulaWorld;
 
 #endregion
 
 namespace NebulaNetwork.PacketProcessors.Factory.Belt;
 
 [RegisterPacketProcessor]
-internal class BeltReverseProcessor : BasePacketProcessor<BeltReversePacket>
+internal class BeltReverseProcessor : BasePacketProcessor<BeltReverseRequest>
 {
-    public override void ProcessPacket(BeltReversePacket packet, INebulaConnection conn)
+    public override void ProcessPacket(BeltReverseRequest packet, INebulaConnection conn)
     {
         var factory = GameMain.galaxy.PlanetById(packet.PlanetId).factory;
         if (factory == null)
@@ -20,28 +23,40 @@ internal class BeltReverseProcessor : BasePacketProcessor<BeltReversePacket>
             return;
         }
 
+        if (IsHost)
+        {
+            var starId = packet.PlanetId / 100;
+            Multiplayer.Session.Server.SendPacketToStar(packet, starId);
+        }
+
         using (NebulaModAPI.MultiplayerSession.Factories.IsIncomingRequest.On())
         {
             NebulaModAPI.MultiplayerSession.Factories.EventFactory = factory;
             NebulaModAPI.MultiplayerSession.Factories.TargetPlanet = packet.PlanetId;
-            if (NebulaModAPI.MultiplayerSession.LocalPlayer.IsHost)
-            {
-                // Load planet model
-                NebulaModAPI.MultiplayerSession.Factories.AddPlanetTimer(packet.PlanetId);
-            }
+            NebulaModAPI.MultiplayerSession.Factories.PacketAuthor = packet.AuthorId;
 
             var beltWindow = UIRoot.instance.uiGame.beltWindow;
-            beltWindow._Close(); // close the window first to avoid changing unwant variable when setting beltId
-            var tmpFactory = beltWindow.factory;
-            var tmpBeltId = beltWindow.beltId;
-            beltWindow.factory = factory;
-            beltWindow.beltId = packet.BeltId;
-            beltWindow.OnReverseButtonClick(0);
-            beltWindow.factory = tmpFactory;
-            beltWindow.beltId = tmpBeltId;
+            try
+            {
+                beltWindow._Close(); // close the window first to avoid changing unwant variable when setting beltId
+                beltWindow.factory = factory;
+                beltWindow.traffic = factory.cargoTraffic;
+                beltWindow.player = GameMain.mainPlayer;
+                beltWindow.beltId = packet.BeltId;
+                beltWindow.OnReverseButtonClick(0);
+                beltWindow._Close();
+            }
+            catch (Exception e)
+            {
+                Log.Warn(e);
+                beltWindow._tmp_ids.Clear();
+                beltWindow._tmp_cargos.Clear();
+                beltWindow._Close();
+            }
 
             NebulaModAPI.MultiplayerSession.Factories.EventFactory = null;
             NebulaModAPI.MultiplayerSession.Factories.TargetPlanet = NebulaModAPI.PLANET_NONE;
+            NebulaModAPI.MultiplayerSession.Factories.PacketAuthor = NebulaModAPI.AUTHOR_NONE;
         }
     }
 }
