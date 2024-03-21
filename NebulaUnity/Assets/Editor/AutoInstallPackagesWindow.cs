0926c2fe0f667a69b0cf3c3b16c69c1356925c46
Sunny Kim <starwin@gmail.com>
2024-03-21 18:38:30 +0900

    attempt version bump

--- NebulaUnity/Assets/Editor/AutoInstallPackagesWindow.cs
+++ NebulaUnity/Assets/Editor/AutoInstallPackagesWindow.cs
@@ -11,7 +11,7 @@ public class AutoInstallPackagesWindow : EditorWindow
 {
     public static string[] Packages =
     {
-        "CommonAPI-CommonAPI-1.6.2", 
+        "CommonAPI-CommonAPI-1.6.5", 
         "CommonAPI-DSPEditorKit-1.0.3"
     };
 
