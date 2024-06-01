a98e4c34e4194f1e189cea422f576fd4df3b45d1
starfish <50672801+starfi5h@users.noreply.github.com>
2024-06-02 02:22:58 +0800

    Add  ILSUpdateRoute packet

---
+++ NebulaNetwork/PacketProcessors/Logistics/ILSUpdateRouteProcessor.cs
@@ -0,0 +1,55 @@
+﻿#region
+
+using NebulaAPI.Packets;
+using NebulaModel.Logger;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Logistics;
+using NebulaWorld;
+
+#endregion
+
+namespace NebulaNetwork.PacketProcessors.Logistics;
+
+[RegisterPacketProcessor]
+public class ILSUpdateRouteProcessor : PacketProcessor<ILSUpdateRoute>
+{
+    protected override void ProcessPacket(ILSUpdateRoute packet, NebulaConnection conn)
+    {
+        Log.Debug($"{packet.Type} id0:{packet.Id0} id1:{packet.Id1}");
+        using (Multiplayer.Session.Ships.PatchLockILS.On())
+        {
+            var galacticTransport = GameMain.data.galacticTransport;
+            switch (packet.Type)
+            {
+                case ILSUpdateRoute.ERouteEvent.AddStation2StationRoute:
+                    galacticTransport.AddStation2StationRoute(packet.Id0, packet.Id1);
+                    break;
+
+                case ILSUpdateRoute.ERouteEvent.RemoveStation2StationRoute_Single:
+                    galacticTransport.RemoveStation2StationRoute(packet.Id0);
+                    break;
+
+                case ILSUpdateRoute.ERouteEvent.RemoveStation2StationRoute_Pair:
+                    galacticTransport.RemoveStation2StationRoute(packet.Id0, packet.Id1);
+                    break;
+
+                case ILSUpdateRoute.ERouteEvent.AddAstro2AstroRoute:
+                    galacticTransport.AddAstro2AstroRoute(packet.Id0, packet.Id1, packet.ItemId);
+                    break;
+
+                case ILSUpdateRoute.ERouteEvent.RemoveAstro2AstroRoute:
+                    galacticTransport.RemoveAstro2AstroRoute(packet.Id0, packet.Id1, packet.ItemId);
+                    break;
+
+                case ILSUpdateRoute.ERouteEvent.SetAstro2AstroRouteEnable:
+                    galacticTransport.SetAstro2AstroRouteEnable(packet.Id0, packet.Id1, packet.ItemId, packet.Enable);
+                    break;
+
+                case ILSUpdateRoute.ERouteEvent.SetAstro2AstroRouteComment:
+                    galacticTransport.SetAstro2AstroRouteComment(packet.Id0, packet.Id1, packet.ItemId, packet.Comment);
+                    break;
+            }
+        }
+    }
+}
