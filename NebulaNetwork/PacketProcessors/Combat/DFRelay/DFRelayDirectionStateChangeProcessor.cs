6a5f111c826762429f24d9caecdbecf5b3285e96
AlienXAXS <github@smtpd.co.uk>
2024-07-18 16:17:36 +0100

    Enemy Relay Direction Sync

---
+++ NebulaNetwork/PacketProcessors/Combat/DFRelay/DFRelayDirectionStateChangeProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NebulaAPI.Packets;
+using NebulaModel.Logger;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Combat.DFRelay;
+using UnityEngine;
+
+namespace NebulaNetwork.PacketProcessors.Combat.DFRelay
+{
+    [RegisterPacketProcessor]
+    public class DFRelayDirectionStateChangeProcessor : PacketProcessor<DFRelayDirectionStateChangePacket>
+    {
+        protected override void ProcessPacket(DFRelayDirectionStateChangePacket packet, NebulaConnection conn)
+        {
+            var hiveSystem = GameMain.spaceSector.GetHiveByAstroId(packet.HiveAstroId);
+            if (hiveSystem == null) return;
+            
+            var dfRelayComponent = hiveSystem.relays.buffer[packet.RelayId];
+            if (dfRelayComponent?.id != packet.RelayId) return;
+
+            Log.Debug($"Relay {dfRelayComponent.id} direction:{dfRelayComponent.direction} will be sent home with a new direction: {packet.NewDirection}");
+
+            switch (packet.NewDirection)
+            {
+                case -1: //Relay is being sent home
+                    dfRelayComponent.targetAstroId = 0;
+                    dfRelayComponent.targetLPos = Vector3.zero;
+                    dfRelayComponent.targetYaw = 0f;
+                    dfRelayComponent.baseState = 0;
+                    dfRelayComponent.baseId = 0;
+                    dfRelayComponent.baseTicks = 0;
+                    dfRelayComponent.baseEvolve = default(EvolveData);
+                    dfRelayComponent.baseRespawnCD = 0;
+                    dfRelayComponent.direction = -1;
+                    dfRelayComponent.param0 = 0f;
+
+                    dfRelayComponent.stage = packet.Stage;
+
+                    Log.Debug($"Relay {dfRelayComponent.id} returning home");
+                    break;
+            }
+        }
+    }
+}
