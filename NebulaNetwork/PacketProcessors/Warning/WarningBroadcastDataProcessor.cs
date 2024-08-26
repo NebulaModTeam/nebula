6cc1e674a5bda89f5fe15ee5240f2eea25a88263
starfish <50672801+starfi5h@users.noreply.github.com>
2024-08-26 16:00:58 +0800

    Sync WarningBroadcastData

---
+++ NebulaNetwork/PacketProcessors/Warning/WarningBroadcastDataProcessor.cs
@@ -0,0 +1,38 @@
+﻿#region
+
+using NebulaAPI.DataStructures;
+using NebulaAPI.Packets;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Warning;
+using NebulaWorld;
+using UnityEngine;
+
+#endregion
+
+namespace NebulaNetwork.PacketProcessors.Warning;
+
+[RegisterPacketProcessor]
+internal class WarningBroadcastDataProcessor : PacketProcessor<WarningBroadcastDataPacket>
+{
+    protected override void ProcessPacket(WarningBroadcastDataPacket packet, NebulaConnection conn)
+    {
+        using (Multiplayer.Session.Warning.IsIncomingBroadcast.On())
+        {
+            var vocal = (EBroadcastVocal)packet.Vocal;
+            var factoryIndex = GameMain.data.galaxy.astrosFactory[packet.AstroId]?.index ?? -1;
+            var astroId = packet.AstroId;
+            var content = packet.Content;
+            var lpos = packet.Lpos.ToVector3();
+
+            if (lpos == Vector3.zero)
+            {
+                GameMain.data.warningSystem.Broadcast(vocal, factoryIndex, astroId, content);
+            }
+            else
+            {
+                GameMain.data.warningSystem.Broadcast(vocal, factoryIndex, astroId, content, lpos);
+            }
+        }
+    }
+}
--- NebulaPatcher/Patches/Dynamic/WarningSystem_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/WarningSystem_Patch.cs
@@ -1,8 +1,11 @@
 ﻿#region
 
+using System;
+using System.Collections.Generic;
 using HarmonyLib;
 using NebulaModel.Packets.Warning;
 using NebulaWorld;
+using UnityEngine;
 
 #endregion
 
@@ -12,8 +15,8 @@ namespace NebulaPatcher.Patches.Dynamic;
 internal class WarningSystem_Patch
 {
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(WarningSystem), nameof(WarningSystem.RemoveWarningData))]
-    [HarmonyPatch(typeof(WarningSystem), nameof(WarningSystem.WarningLogic))]
+    [HarmonyPatch(nameof(WarningSystem.RemoveWarningData))]
+    [HarmonyPatch(nameof(WarningSystem.WarningLogic))]
     public static bool AlterWarningData_Prefix()
     {
         //Let warningPool only be updated by packet
@@ -21,7 +24,7 @@ internal class WarningSystem_Patch
     }
 
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(WarningSystem), nameof(WarningSystem.NewWarningData))]
+    [HarmonyPatch(nameof(WarningSystem.NewWarningData))]
     public static void NewWarningData_Prefix(WarningSystem __instance, ref int factoryId)
     {
         if (!Multiplayer.IsActive || Multiplayer.Session.LocalPlayer.IsHost)
@@ -37,7 +40,7 @@ internal class WarningSystem_Patch
     }
 
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(WarningSystem), nameof(WarningSystem.CalcFocusDetail))]
+    [HarmonyPatch(nameof(WarningSystem.CalcFocusDetail))]
     public static void CalcFocusDetail_Prefix(int __0)
     {
         if (__0 == 0 || !Multiplayer.IsActive || Multiplayer.Session.LocalPlayer.IsHost)
@@ -54,5 +57,60 @@ internal class WarningSystem_Patch
         }
         Multiplayer.Session.Network.SendPacket(new WarningDataRequest(WarningRequestEvent.Data));
         Multiplayer.Session.Warning.LastRequestTime = GameMain.gameTick;
+    }
+
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(WarningSystem.CheckRelayAndSeed))]
+    [HarmonyPatch(nameof(WarningSystem.CheckShieldCollapse))]
+    public static bool DisableCheckInClient()
+    {
+        return !Multiplayer.IsActive || Multiplayer.Session.IsServer;
+    }
+
+    static readonly HashSet<EBroadcastVocal> syncBroadcasts =
+    [
+        EBroadcastVocal.LandingRelay,
+        EBroadcastVocal.PlanetaryShieldDown,
+        EBroadcastVocal.ApproachingSeed,
+        EBroadcastVocal.BuildingDestroyed,
+        EBroadcastVocal.MineralDepleted,
+        EBroadcastVocal.OilSeepDepleted
+    ];
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(WarningSystem.Broadcast), new Type[] { typeof(EBroadcastVocal), typeof(int), typeof(int), typeof(int), typeof(Vector3) })]
+    public static bool Broadcast_Prefix(WarningSystem __instance, EBroadcastVocal vocal, int factoryIndex, int astroId, int context, Vector3 lpos)
+    {
+        if (!Multiplayer.IsActive || !syncBroadcasts.Contains(vocal)) return true;
+
+        // In client, let server authorize the trigger of those sync broadcast
+        if (Multiplayer.Session.IsClient) return Multiplayer.Session.Warning.IsIncomingBroadcast.Value;
+        lock (__instance.broadcasts)
+        {
+            if (__instance.IndexOf(vocal, factoryIndex, astroId, context) < 0) // new data
+            {
+                Multiplayer.Session.Server.SendPacket(new WarningBroadcastDataPacket(vocal, astroId, context, lpos));
+            }
+        }
+        return true;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(WarningSystem.Broadcast), new Type[] { typeof(EBroadcastVocal), typeof(int), typeof(int), typeof(int) })]
+    public static bool Broadcast_Prefix(WarningSystem __instance, EBroadcastVocal vocal, int factoryIndex, int astroId, int context)
+    {
+        if (!Multiplayer.IsActive || !syncBroadcasts.Contains(vocal)) return true;
+
+        // In client, let server authorize the trigger of those sync broadcast
+        if (Multiplayer.Session.IsClient) return Multiplayer.Session.Warning.IsIncomingBroadcast.Value;
+        lock (__instance.broadcasts)
+        {
+            if (__instance.IndexOf(vocal, factoryIndex, astroId, context) < 0) // new data
+            {
+                Multiplayer.Session.Server.SendPacket(new WarningBroadcastDataPacket(vocal, astroId, context, Vector3.zero));
+            }
+        }
+        return true;
     }
 }
