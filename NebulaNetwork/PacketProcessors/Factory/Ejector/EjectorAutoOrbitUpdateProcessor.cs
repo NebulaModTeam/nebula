51d8beba5528b26726cdbdfef0ffc55553911701
starfish <50672801+starfi5h@users.noreply.github.com>
2025-01-25 07:43:25 +0800

    Sync ejector auto orbit settings

---
+++ NebulaNetwork/PacketProcessors/Factory/Ejector/EjectorAutoOrbitUpdateProcessor.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using NebulaAPI.Packets;
+using NebulaModel.Networking;
+using NebulaModel.Packets;
+using NebulaModel.Packets.Factory.Ejector;
+
+#endregion
+
+namespace NebulaNetwork.PacketProcessors.Factory.Ejector;
+
+[RegisterPacketProcessor]
+internal class EjectorAutoOrbitUpdateProcessor : PacketProcessor<EjectorAutoOrbitUpdatePacket>
+{
+    protected override void ProcessPacket(EjectorAutoOrbitUpdatePacket packet, NebulaConnection conn)
+    {
+        var ejectorPool = GameMain.galaxy.PlanetById(packet.PlanetId)?.factory?.factorySystem?.ejectorPool;
+        if (ejectorPool == null) return;
+
+        if (packet.EjectorIndex == -1) // Set whole planet ejector event
+        {
+            for (var i = 1; i < ejectorPool.Length; i++)
+            {
+                if (ejectorPool[i].id == i) ejectorPool[i].autoOrbit = packet.AutoOrbit;
+            }
+        }
+        else if (packet.EjectorIndex > 0 && packet.EjectorIndex < ejectorPool.Length)
+        {
+            ejectorPool[packet.EjectorIndex].autoOrbit = packet.AutoOrbit;
+        }
+    }
+}
