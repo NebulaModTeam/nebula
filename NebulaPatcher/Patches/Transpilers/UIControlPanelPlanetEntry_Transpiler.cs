c24a1e38cd385af01c39e6b176b1c59450b097ee
starfish <50672801+starfi5h@users.noreply.github.com>
2025-02-13 07:43:36 +0800

    Fix compilation for 0.10.32.25595

---
+++ NebulaPatcher/Patches/Transpilers/UIControlPanelPlanetEntry_Transpiler.cs
@@ -0,0 +1,43 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NebulaModel.Logger;
+
+#endregion
+
+namespace NebulaPatcher.Patches.Transpilers;
+
+[HarmonyPatch(typeof(UIControlPanelPlanetEntry))]
+public static class UIControlPanelPlanetEntry_Transpiler
+{
+    [HarmonyTranspiler]
+    [HarmonyPatch(nameof(UIControlPanelPlanetEntry.UpdateBanner))]
+    private static IEnumerable<CodeInstruction> RefreshSubEntries_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        // Change: this.planet.factory.gameData.mainPlayer.uPosition
+        // To:     GameMain.player.uPosition
+        try
+        {
+            var codeMacher = new CodeMatcher(instructions)
+                .MatchForward(false,
+                    new CodeMatch(OpCodes.Ldarg_0),
+                    new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "planet"),
+                    new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "factory"),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_gameData"),
+                    new CodeMatch(i => i.opcode == OpCodes.Callvirt && ((MethodInfo)i.operand).Name == "get_mainPlayer")
+                )
+                .RemoveInstructions(4)
+                .SetAndAdvance(OpCodes.Call, AccessTools.Method(typeof(GameMain), "get_mainPlayer"));
+
+            return codeMacher.InstructionEnumeration();
+        }
+        catch
+        {
+            Log.Error("Transpiler UIControlPanelPlanetEntry.UpdateBanner error.");
+            return instructions;
+        }
+    }
+}
