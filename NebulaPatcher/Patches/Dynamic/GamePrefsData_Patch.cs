56f3dc294c32994f560d7a847feb000220afa988
starfish <50672801+starfi5h@users.noreply.github.com>
2026-01-31 19:22:23 +0800

    Update MultiplayerOptions ShowDetail group

---
+++ NebulaPatcher/Patches/Dynamic/GamePrefsData_Patch.cs
@@ -0,0 +1,37 @@
+﻿#region
+
+using HarmonyLib;
+using NebulaModel;
+using NebulaWorld;
+
+#endregion
+
+namespace NebulaPatcher.Patches.Dynamic;
+
+[HarmonyPatch(typeof(GamePrefsData))]
+internal class GamePrefsData_Patch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(GamePrefsData.Restore))]
+    public static void Restore_Postfix()
+    {
+        if (!Multiplayer.IsActive || Multiplayer.Session.LocalPlayer.IsHost)
+        {
+            return;
+        }
+
+        NebulaModel.Logger.Log.Debug("Apply save prefs");
+        var uiGame = UIRoot.instance.uiGame;
+        PowerSystemRenderer.powerGraphOn = Config.Options.ShowDetailPowerGrid;
+        uiGame.dfVeinOn = Config.Options.ShowDetailVeinDistribution;
+        uiGame.dfSpaceGuideOn = Config.Options.ShowDetailSpaceNavigation;
+        DefenseSystemRenderer.turretGraphOn = Config.Options.ShowDetailDefenseArea;
+        EntitySignRenderer.showSign = Config.Options.ShowDetailBuildingAlarm;
+        EntitySignRenderer.showIcon = Config.Options.ShowDetailBuildingIcon;
+        PostEffectController.headlight = Config.Options.ShowGuidingLight;
+        if (GameMain.sectorModel != null)
+        {
+            GameMain.sectorModel.disableHPBars = !Config.Options.ShowDetailHpBars;
+        }
+    }
+}
